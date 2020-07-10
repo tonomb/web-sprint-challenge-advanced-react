ae7a28c6b8f98764ccc8c08c8c34ea4afa91d4dc
antonio martinez baez <tono.mtzb@gmail.com>
2020-07-10 11:05:22 -0500

    tests passing, completed mvp

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,45 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+    //passed sanity check when looking for wrong header
+    const formHeader = screen.getByText(/checkout form/i);
+
+    expect(formHeader).toBeInTheDocument()
+
+});
+
+test("form shows success message on submit with form details", () => {
+    
+    // == arrange ==
+    render(<CheckoutForm />)
+
+    const fNameInput = screen.getByLabelText(/first name/i)
+    const lNameInput = screen.getByLabelText(/last name/i)
+    const addressInput = screen.getByLabelText(/address/i)
+    const cityInput = screen.getByLabelText(/city/i)
+    const stateInput = screen.getByLabelText(/state/i)
+    const zipInput = screen.getByLabelText(/zip/i)
+    const submitBtn = screen.getByRole('button')
+
+    
+    // == act ==
+    fireEvent.change(fNameInput, {target: {value: 'Antonio'}})
+    fireEvent.change(lNameInput, {target: {value: 'Martinez Baez'}})
+    fireEvent.change(addressInput, {target: {value: 'valladolid 21'}})
+    fireEvent.change(cityInput, {target: {value: 'Mexico'}})
+    fireEvent.change(stateInput, {target: {value: 'Mexico'}})
+    fireEvent.change(zipInput, {target: {value: '06700'}})
+    fireEvent.click(submitBtn)
+
+    const successMessage = screen.getByTestId('successMessage');
+
+    expect(successMessage).toBeInTheDocument();
+
+
+});
